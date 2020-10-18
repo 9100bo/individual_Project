3cc23e33acc78792740a36bef0d7bc3c86b2acbc
9100bo <65898377+9100bo@users.noreply.github.com>
2020-10-19 01:26:10 +0800

    10-19

--- js/script.js
+++ js/script.js
@@ -10,4 +10,16 @@ menuIcon.addEventListener('click', e => {
   menuIconClose.classList.toggle("fa-times");
 });
 
-/* Concept */
+/* Staff */
+const horizontalAccordions = $(".accordion.width");
+
+horizontalAccordions.each((index, element) => {
+	const accordion = $(element);
+  const collapse = accordion.find(".collapse");
+  const bodies = collapse.find("> *");
+  accordion.height(accordion.height());  
+  bodies.width(bodies.eq(0).width());
+  collapse.not(".show").each((index, element) => {
+  	$(element).parent().find("[data-toggle='collapse']").addClass("collapsed");
+  });
+});
